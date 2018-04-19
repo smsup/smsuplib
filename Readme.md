ed8a38b54abed969a7ebc3144825aa9a0ffa5dc9
ale-blanco <ablancosoft@gmail.com>
2018-04-19 12:23:58 +0200

    Commit inicial

---
+++ Readme.md
@@ -0,0 +1,84 @@
+smsuplib
+============
+
+Libreria nodejs que facilita el uso de la api de smsup.es para el envio de sms.
+
+Documentacion de la API disponible en:
+
+http://www.smsup.es/docs/api/
+
+Uso
+---
+
+Envio de un sms:
+
+``` javascript
+sms.sendSms('texto del mensaje', ['+34 600000000'], 'Remitente')
+    .then(
+        function (response) {
+            console.log('OK', response);
+        },
+        function (response) {
+            console.log(response);
+        }
+    );
+```
+
+Si todo ha ido bien, en response se recibe un array de objetos, uno por telefono indicado. 
+Cada objeto tiene dos parametros:
+- telefono: telefono indicado al hacer la peticion.
+- id: id asignado a ese envio. Es el id ha usar en otras peticiones, por ejemplo para saber el estado del envio.
+
+Si ha ocurrido un error, se recibe en response un objeto con dos parametros:
+- httCode: codigo http de la respuesta.
+- error: mensaje explicativo del error
+
+Ademas de los tres parametros indicados, el metodo sendSms admite otros 3 parametros opcionales:
+- date: objeto Date con la fecha en la que se quiere enviar el mensaje. Si no se indica se envia en el momento actual.
+- coding: codificacion usada para el mensaje smsuplib.GSM, smsuplib.UNICODE o smsuplib.AUTO
+- reference: identificador unico de esa peticion
+
+Obtener el estado de un envio:
+
+``` javascript
+sms.stateSms('1000000')
+    .then(
+        function (response) {
+            console.log('OK', response);
+        },
+        function (response) {
+            console.log(response);
+        }
+    );
+```
+
+Si la peticion es correcta, se recibe un objeto con tres parametros:
+- estado: estado del envio.
+- mensajes: numero de mensajes necesarios para realizar el envio.
+- creditos: numero de creditos consumidos por este envio.
+
+En caso de error la respuesta es similar a la del metodo anterior.
+
+Creditos disponibles:
+
+``` javascript
+sms.credits()
+    .then(
+        function (response) {
+            console.log('OK', response);
+        },
+        function (response) {
+            console.log(response);
+        }
+    );
+```
+
+Con este metodo podemos obtener el numero de creditos disponibles en nuestra cuenta. 
+Se recibe un objeto con un solo parametro, creditos, con el numero de creditos disponible.
+
+
+Otros metodos:
+
+Existen otros metodos disponibles, con un uso similar a los anteriores, que son:
+- deleteSms: elimina un envio, si aun no ha llegado la fecha de envio. Recibe como parametro el id del sms recibido en la peticiion de envio.
+- resultRequest: nos devuelve el resultado de una peticion de envio anterior, si se indico la referencia, por si no fue posible procesarla en su momento. Recibe como parametro la referencia.
---
+++ index.js
@@ -0,0 +1,97 @@
+module.exports = function(ID, SECRET) {
+    var URL = 'http://smsupp.local';
+
+    var PATH_SMS = '/api/sms/';
+    var PATH_CREDITS = '/api/creditos/';
+    var PATH_REQUEST = '/api/peticion/';
+
+    var POST = 'POST';
+    var DELETE = 'DELETE';
+    var GET = 'GET';
+
+    var smsuplib = {};
+
+    smsuplib.GSM = 'gsm';
+    smsuplib.UNICODE = 'uni';
+    smsuplib.AUTO = 'aut';
+
+    smsuplib.sendSms = function (text, numbers, sender, date, coding, reference) {
+        date = (date instanceof Date) ? date.toISOString() : 'NOW';
+        reference = reference || '';
+        var post = {texto: text, fecha: date, telefonos: numbers, referencia: reference, remitente: sender};
+        if (coding != null && [smsuplib.GSM, smsuplib.UNICODE, smsuplib.AUTO].indexOf(coding) > -1) {
+            post.codificacion = coding;
+        }
+        var headers = createHeaders(PATH_SMS, POST, post);
+        return sendRequest(PATH_SMS, POST, headers, post);
+    };
+
+    smsuplib.deleteSms = function (idSms) {
+        var path = PATH_SMS + idSms + '/';
+        var headers = createHeaders(path, DELETE);
+        return sendRequest(path, DELETE, headers);
+    };
+
+    smsuplib.stateSms = function(idSms) {
+        var path = PATH_SMS + idSms + '/';
+        var headers = createHeaders(path, GET);
+        return sendRequest(path, GET, headers);
+    };
+
+    smsuplib.credits = function () {
+        var headers = createHeaders(PATH_CREDITS, GET);
+        return sendRequest(PATH_CREDITS, GET, headers);
+    };
+
+    smsuplib.resultRequest = function(reference) {
+        var path = PATH_REQUEST + reference + '/';
+        var headers = createHeaders(path, GET);
+        return sendRequest(path, GET, headers);
+    };
+
+
+
+    function createHeaders(url, method, postData) {
+        var headers = {};
+        var smsdate = (new Date()).toISOString();
+        headers['Sms-Date'] = smsdate;
+
+        var textPost = (postData) ? JSON.stringify(postData) : '';
+        var text = method + url + smsdate + textPost;
+        hash = hash_hmac(text, SECRET);
+        headers['Firma'] = ID + ':' + hash;
+        return headers;
+    }
+
+    function hash_hmac(text, key) {
+        var crypto = require('crypto');
+        var buf1 = crypto.createHmac("sha1", key).update(text).digest();
+        return buf1.toString('hex');
+    }
+
+    function sendRequest(url, method, header, body) {
+        var request = require('request');
+        var options = {
+            url: URL + url,
+            headers: header,
+            method: method,
+            json: body
+        };
+        return new Promise(function (resolve, reject) {
+            request(options,
+                function (error, response, body) {
+                    if (response.statusCode !== 200) {
+                        body.httpCode = response.statusCode;
+                        reject(body);
+                        return;
+                    }
+
+                    resolve(body);
+                }
+            );
+        });
+
+    }
+
+    return smsuplib;
+};
